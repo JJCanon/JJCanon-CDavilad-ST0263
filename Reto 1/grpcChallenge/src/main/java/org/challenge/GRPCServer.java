ea311bfb5b87945809c799fed8b435961c3b12aa
JJCanon <jjcanond@eafit.edu.co>
2024-09-13 21:18:39 -0500

    package .jar and changes in pom.xml

--- Reto 1/grpcChallenge/src/main/java/org/challenge/GRPCServer.java
+++ Reto 1/grpcChallenge/src/main/java/org/challenge/GRPCServer.java
@@ -9,10 +9,10 @@ public class GRPCServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server = ServerBuilder.forPort(50051)
                 .addService(new GreeterImpl())
-                .build();
-
-        server.start();
+                .build()
+                .start();
         System.out.println("Servidor gRPC iniciado en el puerto 50051");
         server.awaitTermination();
     }
+
 }
