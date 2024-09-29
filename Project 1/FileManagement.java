300b8a49647587cb10e82c966dbfc4ed5238dca9
JJCanon <jjcanond@eafit.edu.co>
2024-09-29 15:34:46 -0500

    maven and java class to management files

---
+++ Project 1/FileManagement.java
@@ -0,0 +1,44 @@
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+class FileManagement {
+
+    // Variables
+    String fileName;
+    FileWriter writer;
+
+    // Constructor
+    public FileManagement(String fileName) {
+        this.fileName = fileName;
+    }
+
+    // Functions
+    // This function is to write in a file
+    public void Write(String Data) {
+        // txt
+        try {
+            writer = new FileWriter(fileName);
+            writer.write(Data);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This function is to read in a file
+    public void read() {
+        // Txt
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String Linea;
+            while ((Linea = br.readLine()) != null) {
+                System.out.println(Linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
