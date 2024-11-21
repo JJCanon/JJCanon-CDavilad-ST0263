bceba09850c6f33394d46d0bb4d92eee0ba2972b
JJCanon <jjcanond@eafit.edu.co>
2024-11-21 14:05:04 -0500

    back and front in containers

--- Project3/WebApp/client/src/services/bookService.js
+++ Project3/WebApp/client/src/services/bookService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/books';
+const API_URL = `${process.env.REACT_APP_API_URL}/books`;
 
 export const getBooks = async () => {
     try {
